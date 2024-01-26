09e2305be846fbf457634b61a859f021352216b1
AbdoKousara <abdul.kousara@gmail.com>
2024-01-26 14:42:42 +0100

    fixing the shop cart bug

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,12 +2,12 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
-} from './button.styles';
+} from "./button.styles";
 
 export const BUTTON_TYPE_CLASSES = {
-  base: 'base',
-  google: 'google-sign-in',
-  inverted: 'inverted',
+  base: "base",
+  google: "google-sign-in",
+  inverted: "inverted",
 };
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
@@ -15,7 +15,7 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-  }[buttonType]);
+  })[buttonType];
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
